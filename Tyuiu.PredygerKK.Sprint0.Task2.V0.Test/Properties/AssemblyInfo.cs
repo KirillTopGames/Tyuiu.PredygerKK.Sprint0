2c17e212a4224c043160539fd296db78d55c7845
Tyuiu.PredygerKK@outlook.com <Tyuiu.PredygerKK@outlook.com>
2024-09-14 16:35:42 +0500

    Task2

---
+++ Tyuiu.PredygerKK.Sprint0.Task2.V0.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.PredygerKK.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.PredygerKK.Sprint0.Task2.V0.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("014c03a4-43ab-4543-9d31-305df201a858")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Tyuiu.PredygerKK.Sprint0.Task2.V0/Program.cs
@@ -0,0 +1,11 @@
+﻿using Tyuiu.PredygerKK.Sprint0.Task2.V0.Lib;
+namespace Tyuiu.PredygerKK.Sprint0.Task2.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("Кирилл"));
+        }
+    }
+}
