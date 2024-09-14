c4e19510bf4ab1be07cd0d78cc2c1619de709565
Tyuiu.PredygerKK@outlook.com <Tyuiu.PredygerKK@outlook.com>
2024-09-14 20:08:18 +0500

    Task5

---
+++ Tyuiu.PredygerKK.Sprint0.Task5.V0.Test/DataServiceTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.PredygerKK.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.PredygerKK.Sprint0.Task5.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void CheckedAdditionalValid()
+        {
+            Assert.AreEqual(10, DataService.Additions(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckedMinusValid()
+        {
+            Assert.AreEqual(0, DataService.Minus(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckedMnozhValid()
+        {
+            Assert.AreEqual(25, DataService.Mnozh(5, 5));
+        }
+
+        [TestMethod]
+        public void CheckeddeleValid()
+        {
+            Assert.AreEqual(1, DataService.Dele(5, 5));
+        }
+    }
+
+}
---
+++ Tyuiu.PredygerKK.Sprint0.Task5.V0/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Tyuiu.PredygerKK.Sprint0.Task5.V0.Lib;
+
+namespace Tyuiu.PredygerKK.Sprint0.Task5.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("A + B = "+DataService.Additions(1, 5));
+            Console.WriteLine("A - B = " + DataService.Minus(1000, 7));
+            Console.WriteLine("A * B = " + DataService.Mnozh(625000, 2));
+            Console.WriteLine("A / B = " + DataService.Dele(5, 0));
+        }
+    }
+}
